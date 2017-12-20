0234b4002cd066808b425ba59854573d6f7494c0
Cèsar <Csar@172.16.2.185>
2017-12-20 19:29:58 -0300

    Funcionando Agrega,Busca y modifica

--- src/ventaservicio/modelo/DetalleVenta.java
+++ src/ventaservicio/modelo/DetalleVenta.java
@@ -14,5 +14,49 @@ public class DetalleVenta {
     private int idProducto;
     private int precio;
     private int cantidad;
+
+    public DetalleVenta() {
+    }
+
+    public DetalleVenta(int idVenta, int idProducto, int precio, int cantidad) {
+        this.idVenta = idVenta;
+        this.idProducto = idProducto;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    public int getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(int idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    
     
 }
--- src/ventaservicio/modelo/Venta.java
+++ src/ventaservicio/modelo/Venta.java
@@ -19,6 +19,66 @@ public class Venta {
     private int tipoVenta;
     private int valorVenta;
     private String rut;
+
+    public Venta() {
+    }
+
+    public Venta(int idVenta, Date fechaContrato, Date fechaTermino, int tipoVenta, int valorVenta, String rut) {
+        this.idVenta = idVenta;
+        this.fechaContrato = fechaContrato;
+        this.fechaTermino = fechaTermino;
+        this.tipoVenta = tipoVenta;
+        this.valorVenta = valorVenta;
+        this.rut = rut;
+    }
+
+    public int getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(int idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public Date getFechaContrato() {
+        return fechaContrato;
+    }
+
+    public void setFechaContrato(Date fechaContrato) {
+        this.fechaContrato = fechaContrato;
+    }
+
+    public Date getFechaTermino() {
+        return fechaTermino;
+    }
+
+    public void setFechaTermino(Date fechaTermino) {
+        this.fechaTermino = fechaTermino;
+    }
+
+    public int getTipoVenta() {
+        return tipoVenta;
+    }
+
+    public void setTipoVenta(int tipoVenta) {
+        this.tipoVenta = tipoVenta;
+    }
+
+    public int getValorVenta() {
+        return valorVenta;
+    }
+
+    public void setValorVenta(int valorVenta) {
+        this.valorVenta = valorVenta;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
     
     
 }
