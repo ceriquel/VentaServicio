84ab0402b281c39e9a90e1c2347b4d2a67f4a60b
Cèsar <Csar@172.16.2.185>
2017-12-20 22:16:37 -0300

    Funcionando Agrega,Busca y modifica

--- src/ventaservicio/controlador/ControladorAgregar.java
+++ src/ventaservicio/controlador/ControladorAgregar.java
@@ -5,10 +5,23 @@
  */
 package ventaservicio.controlador;
 
+import java.sql.Date;
+import java.sql.SQLException;
+import ventaservicio.modelo.ModeloAgregar;
+
 /**
  *
  * @author Cèsar
  */
 public class ControladorAgregar {
+    
+     private ModeloAgregar modeloAgregar;
+     
+     // public boolean agregarVenta(String tipo_venta, String producto, int precio, int cantidad, Date fechacontra, Date fechaterm,Date fechafinc,int rut, int valorventa) throws SQLException{
+       // modeloAgregar=new ModeloAgregar();
+        //return modeloAgregar.agregarVenta(tipo_venta, producto, precio, cantidad, fechacontra, fechaterm,fechafinc,rut,valorventa);
+    //}
+    
+
     
 }
