80d1767b77eb9338dc756e4f8f54b32511eff3a0
Rodrigo Perez <Rodrigo Perez@Rodrigo-PC>
2017-12-20 22:11:57 -0300

    primer avance

--- src/ventaservicio/modelo/ModeloAgregar.java
+++ src/ventaservicio/modelo/ModeloAgregar.java
@@ -5,10 +5,26 @@
  */
 package ventaservicio.modelo;
 
+import java.sql.Date;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author Cèsar
  */
 public class ModeloAgregar {
+    
+    public boolean agregarPelicula(String tipo_venta, String producto, int precio, int cantidad, Date fechacontra, Date fechaterm,Date fechafinc,int rut, int valorventa) throws SQLException{
+        boolean flag=false;
+        System.out.println("Insertar registro...");
+        Statement dec;
+        dec=Conexion.conexion.createStatement();
+        String query1="insert into VENTA (ID_VENTA, FECHA_CONTRATACION, FECHA_TERMINO_OPCIONAL, FECHA_TERMINO_CONTRATO, TIPO_VENTA_ID_TIPO, VALOR_VENTA) VALUES ("+precio+", (SELECT ID_CATEGORIA FROM CATEGORIA WHERE NOMBRE_CATEGORIA='"+categoria+"'), '"+formato4k+"', '"+nombre+"')";
+        dec.executeUpdate(query1);
+        return flag=true;    
+    }
+    
+    
     
 }
